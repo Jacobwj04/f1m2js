82c44fd5568389ba3e39eac946a3920f88564162
Jacobwj04 <32894@ma-web.nl>
2021-11-29 12:48:43 +0100

    Les2 Boodschappen

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,17 @@
 console.log("script is geladen!");
+
+const groentevak = document.getElementById('groente')
+const fruitvak = document.getElementById('fruit')
+
+function ZetinFruitla(fruit){
+    console.log('fruit')
+
+    const nieuweElement = document.createElement('h2')
+    nieuweElement.innerHTML = fruit;
+    fruitvak.appendChild(nieuweElement);
+    nieuweElement.className = 'rood';
+}
+
+function ZetinGroentela(groente){
+    console.log('groente')
+}
